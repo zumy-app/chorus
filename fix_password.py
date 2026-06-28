9c1b3a664b4bc1f03cd49b945d6b8dc9ea5c0b53
Prashanth Batchu <uhsarp@gmail.com>
2026-06-28 12:18:27 -0400

    Remove temp files from tracking

--- fix_password.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python3
-import os
-os.chdir('/root/chorus')
-with open('.env', 'r') as f:
-    c = f.read()
-c = c.replace('ChorusProd2026!', 'ChorusProd2026x')
-with open('.env', 'w') as f:
-    f.write(c)
-print('Password updated to ChorusProd2026x')
